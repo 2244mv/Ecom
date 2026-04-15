9303251d9cbe524f20c1b59ba04ac6fc7eda471a
Manish Verma <manishverma8244@gmail.com>
2026-04-15 23:56:38 +0530

    apifig.js

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", 
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5000", 
+//   timeout: 5000,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-export default axiosInstance;
+//  export default axiosInstance;
